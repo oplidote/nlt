1baf11ae8aff6ab28dc22774b5519fa674dd5b82
jhwmac <jkr2505@naver.com>
2022-03-28 11:30:40 +0900

    전체메뉴 (nav) 추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    // 변수
+
+    // 전체메뉴
+    let $nav_bg = $('.nav-bg');
+    let $nav = $('.nav');
+    let $nav_wrap = $('.nav-wrap');
+    let $nav_close = $('.nav-close');
+    let $menu = $('.menu');
+    // 페이지
+    let $body = $('body');
+    let $page2 = $('.page2');
+    let $page1 = $('.page1');
+    let $gotop = $('.gotop');
+
     // 풀페이지
     $('#fullpage').fullpage({
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
@@ -10,25 +24,46 @@ $(document).ready(function () {
         navigationTooltips: ['HOME', 'ABOUT', 'PRODUCT', 'R&D', 'CUSTOMER'],
         showActiveTooltip: true,
     });
+
     
+    // 전체메뉴
+    $menu.mouseenter(function () {
+        $nav.addClass('nav-on');
+        $nav_bg.stop().fadeIn(300);
+        $('body').on('scroll touchmove mousewheel', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        });
+    });
 
-    let $body = $('body');
-    let $page2 = $('.page2');
-    let $page1 = $('.page1');
-    let $gotop = $('.gotop');
+    $nav_close.click(function () {
+        $nav.removeClass('nav-on');
+        $nav_bg.stop().fadeOut(300);
+        $('body').off('scroll touchmove mousewheel');
+    });
+    $nav_wrap.mouseleave(function(){
+        $nav.removeClass('nav-on');
+        $nav_bg.stop().fadeOut(300);
+        $('body').off('scroll touchmove mousewheel');
+    })
 
+    // 마우스 휠 시 작동
     $(window).bind("mousewheel", function (e) {
         dark();
         gotop();
     });
+
+    // 고탑 기능
     function gotop() {
-        if($page1.hasClass('active')){
+        if ($page1.hasClass('active')) {
             $gotop.fadeOut(300);
-        }
-        else {
+        } else {
             $gotop.fadeIn(300);
         }
     }
+
+    // 헤더영역 2페이지 시 색상 변경
     function dark() {
         if ($page2.hasClass('active') && $body.hasClass('dark') != true) {
             $body.addClass('dark');
@@ -48,7 +83,9 @@ $(document).ready(function () {
             })
         }
     });
-    var page1_swiper = new Swiper(".page1-swiper", {
+
+    // 스와이퍼 슬라이드
+    let page1_swiper = new Swiper(".page1-swiper", {
         effect: 'fade',
         slidesPerview: 1,
         touchRatio: 0,
@@ -60,7 +97,7 @@ $(document).ready(function () {
             clickable: true,
         },
     });
-    var page3_swiper = new Swiper(".page3-swiper", {
+    let page3_swiper = new Swiper(".page3-swiper", {
         loop: true,
         pagination: {
             el: ".page3-pg",
@@ -73,13 +110,14 @@ $(document).ready(function () {
             prevEl: ".page3-prev"
         }
     });
-    var page5_swiper = new Swiper(".page5-swiper", {
-        slidesPerView:3,
+    let page5_swiper = new Swiper(".page5-swiper", {
+        slidesPerView: 3,
         pagination: {
             el: ".page5-pg",
-            clickable:true,
+            clickable: true,
         },
     });
+
     dark();
     gotop();
 });
